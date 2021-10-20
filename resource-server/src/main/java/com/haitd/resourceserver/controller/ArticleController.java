1ae0a418cfe53688d7cf9d1839ec9599d89b51de
Duy Hải Trịnh <haitrinh.se@gmail.com>
2021-10-20 17:45:18 +0700

    add auth server, resource server, client

---
+++ resource-server/src/main/java/com/haitd/resourceserver/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.haitd.resourceserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleController {
+    @GetMapping("/articles")
+    public String[] getArticles() {
+        return new String[] {"Article 1", "Article 2", "Article 3"};
+    }
+}
